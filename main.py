50d636fa504c45231133a4d52a28d6008da22cb0
melikeserin <139703192+melikeserin@users.noreply.github.com>
2025-05-14 13:07:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
